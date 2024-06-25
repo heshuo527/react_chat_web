419c65020b67e5542ac6c89ae23f78c62d1bf624
H_硕 <2393097587@qq.com>
2024-06-25 09:14:08 +0800

    feat: 初始提交

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+import './chat.scss'
+
+const Chat = () => {
+  return (
+    <div className='chat'>Chat</div>
+  )
+}
+
+export default Chat
---
+++ src/components/detail/Detail.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+import './detail.scss'
+
+const Detail = () => {
+  return (
+    <div className='detail'>Detail</div>
+  )
+}
+
+export default Detail
